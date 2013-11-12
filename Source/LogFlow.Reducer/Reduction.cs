986f7d3568fcb7bef3bad2ea66fa929624d402e4
Emil Cardell <emil.cardell@thomascook.se>
2013-11-12 15:02:38 +0100

    Should be a working example by now

--- Source/LogFlow.Reducer/Reduction.cs
+++ Source/LogFlow.Reducer/Reduction.cs
@@ -9,10 +9,10 @@ namespace LogFlow.Reducer
 {
 	public abstract class Reduction<TIn, TOut, THelp> : IReduction where TIn : class  
 	{
-        private const int TimesToRetry = 10;
+		private const int TimesToRetry = 10;
 		private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 		private ReductionStructure<TIn, TOut, THelp> _reductionStructure = new ReductionStructure<TIn, TOut, THelp>();
-        private FlowStatus _currentStatus = FlowStatus.Stopped;
+		private FlowStatus _currentStatus = FlowStatus.Stopped;
 		
 		protected ReductionStructure<TIn, TOut, THelp> CreateReductionWithSettings(Action<ReductionSettings> configureSettings)
 		{
@@ -28,160 +28,160 @@ namespace LogFlow.Reducer
 
 		private ElasticClient _client;
 
-        private CancellationTokenSource _tokenSource;
-        private Task _processTask;
-	    
-        public void Start()
-	    {
-            Log.Info(string.Format("{0}: Starting.", GetType().Name));
-            _tokenSource = new CancellationTokenSource();
-            _processTask = Task.Factory.StartNew(ExecuteProcess, _tokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-	    }
-
-        private void ExecuteProcess()
-        {
-            var retriedTimes = 0;
-
-            _currentStatus = FlowStatus.Running;
-            Log.Info(string.Format("{0}: Started.", GetType().Name));
-
-            while (true)
-            {
-                try
-                {
-                    ExecutePeriod();
-                    _tokenSource.Token.ThrowIfCancellationRequested();
-                }
-                catch (OperationCanceledException)
-                {
-                    _currentStatus = FlowStatus.Stopped;
-                    Log.Info(string.Format("{0}: Stopped.", GetType().Name));
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    if (retriedTimes < TimesToRetry)
-                    {
-                        retriedTimes++;
-                        _currentStatus = FlowStatus.Retrying;
-
-                        Log.Warn(string.Format("{0}: {1}", GetType().Name, ex));
-                        Log.Warn(string.Format("{0}: Retrying {1} times.", GetType().Name, retriedTimes));
-                        Thread.Sleep(TimeSpan.FromSeconds(10));
-                        continue;
-                    }
-
-                    _currentStatus = FlowStatus.Broken;
-
-                    Log.Error(ex);
-                    Log.Error(string.Format("{0}: Shut down because broken!", GetType().Name));
-                    break;
-                }
-
-                if (_currentStatus == FlowStatus.Retrying)
-                {
-                    _currentStatus = FlowStatus.Running;
-
-                    Log.Info(string.Format("{0}: Resuming after {1} times.", GetType().Name, retriedTimes));
-                    retriedTimes = 0;
-                }
-            }
-        }
-
-	    private void ExecutePeriod()
-	    {
-	        var clientSettings =
-	            new ConnectionSettings(
-	                new Uri(string.Format("http://{0}:{1}", _reductionStructure.Settings.Host,
-	                                      _reductionStructure.Settings.Port)));
-	        _client = new ElasticClient(clientSettings);
-
-	        ReductionPeriod period = LoadPeriod(_reductionStructure.Settings);
-
-	        var logLines = _client.Search<TIn>(
-	            s => s.Skip(0).Take(int.MaxValue).Query(q =>
-	                                          q.Range(r =>
-	                                                  r.OnField(ElasticSearchFields.Timestamp)
-	                                                   .From(period.From)
-	                                                   .To(period.To)
-	                                                   .ToExclusive()
-	                                              )));
-
-
-	        var result = new Dictionary<string, ReductionResultData<TOut, THelp>>();
-
-	        Parallel.ForEach(logLines.Documents, () => new Dictionary<string, ReductionResultData<TOut, THelp>>(),
-	                         (record, loopControl, localDictionary) =>
-	                         _reductionStructure.Reducer(record, localDictionary),
-	                         (localDictionary) =>
-	                             {
-	                                 lock (result)
-	                                 {
-	                                     _reductionStructure.Combiner(result, localDictionary);
-	                                 }
-	                             });
-            
-            EnsureIndexExists(_reductionStructure.Settings.IndexName);
-
-	        foreach (var reductionResultData in result)
-	        {
-	            _client.Index(reductionResultData.Value, _indexName, GetType().Name, reductionResultData.Key);
-	        }
-	        _client.Flush();
-
-	        SetNewPeriod(_reductionStructure.Settings, period);
-
-	        if (period.IsCurrent)
-	            Thread.Sleep(TimeSpan.FromSeconds(60));
-
-	    }
-
-	    private void SetNewPeriod(ReductionSettings settings, ReductionPeriod reductionPeriod)
+		private CancellationTokenSource _tokenSource;
+		private Task _processTask;
+		
+		public void Start()
 		{
-            var storageKey = GetPositionStorageKey(settings);
-            _storage.Insert(storageKey, reductionPeriod.To);
+			Log.Info(string.Format("{0}: Starting.", GetType().Name));
+			_tokenSource = new CancellationTokenSource();
+			_processTask = Task.Factory.StartNew(ExecuteProcess, _tokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
 		}
 
-        private readonly StateStorage _storage = new StateStorage("PeriodStateStorageKey");
+		private void ExecuteProcess()
+		{
+			var retriedTimes = 0;
+
+			_currentStatus = FlowStatus.Running;
+			Log.Info(string.Format("{0}: Started.", GetType().Name));
+
+			while (true)
+			{
+				try
+				{
+					ExecutePeriod();
+					_tokenSource.Token.ThrowIfCancellationRequested();
+				}
+				catch (OperationCanceledException)
+				{
+					_currentStatus = FlowStatus.Stopped;
+					Log.Info(string.Format("{0}: Stopped.", GetType().Name));
+					break;
+				}
+				catch (Exception ex)
+				{
+					if (retriedTimes < TimesToRetry)
+					{
+						retriedTimes++;
+						_currentStatus = FlowStatus.Retrying;
+
+						Log.Warn(string.Format("{0}: {1}", GetType().Name, ex));
+						Log.Warn(string.Format("{0}: Retrying {1} times.", GetType().Name, retriedTimes));
+						Thread.Sleep(TimeSpan.FromSeconds(10));
+						continue;
+					}
+
+					_currentStatus = FlowStatus.Broken;
+
+					Log.Error(ex);
+					Log.Error(string.Format("{0}: Shut down because broken!", GetType().Name));
+					break;
+				}
+
+				if (_currentStatus == FlowStatus.Retrying)
+				{
+					_currentStatus = FlowStatus.Running;
+
+					Log.Info(string.Format("{0}: Resuming after {1} times.", GetType().Name, retriedTimes));
+					retriedTimes = 0;
+				}
+			}
+		}
+
+		private void ExecutePeriod()
+		{
+			var clientSettings =
+				new ConnectionSettings(
+					new Uri(string.Format("http://{0}:{1}", _reductionStructure.Settings.Host,
+										  _reductionStructure.Settings.Port)));
+			_client = new ElasticClient(clientSettings);
+
+			ReductionPeriod period = LoadPeriod(_reductionStructure.Settings);
+
+			var logLines = _client.Search<TIn>(
+				s => s.Skip(0).Take(int.MaxValue).Query(q =>
+											  q.Range(r =>
+													  r.OnField(ElasticSearchFields.Timestamp)
+													   .From(period.From)
+													   .To(period.To)
+													   .ToExclusive()
+												  )));
+
+
+			var result = new Dictionary<string, ReductionResultData<TOut, THelp>>();
+
+			Parallel.ForEach(logLines.Documents, () => new Dictionary<string, ReductionResultData<TOut, THelp>>(),
+							 (record, loopControl, localDictionary) =>
+							 _reductionStructure.Reducer(record, localDictionary),
+							 (localDictionary) =>
+								 {
+									 lock (result)
+									 {
+										 _reductionStructure.Combiner(result, localDictionary);
+									 }
+								 });
+			
+			EnsureIndexExists(_reductionStructure.Settings.IndexName);
+
+			foreach (var reductionResultData in result)
+			{
+				_client.Index(reductionResultData.Value, _indexName, GetType().Name, reductionResultData.Key);
+			}
+			_client.Flush();
+
+			SetNewPeriod(_reductionStructure.Settings, period);
+
+			if (period.IsCurrent)
+				Thread.Sleep(TimeSpan.FromSeconds(60));
+
+		}
+
+		private void SetNewPeriod(ReductionSettings settings, ReductionPeriod reductionPeriod)
+		{
+			var storageKey = GetPositionStorageKey(settings);
+			_storage.Insert(storageKey, reductionPeriod.To);
+		}
+
+		private readonly StateStorage _storage = new StateStorage("PeriodStateStorageKey");
 		private ReductionPeriod LoadPeriod(ReductionSettings settings)
 		{
 			var storageKey = GetPositionStorageKey(settings);
-		    var startDate = DateTime.UtcNow.AddDays(-30);
+			var startDate = DateTime.UtcNow.AddDays(-30);
 
-		    try
-		    {
-		        startDate = _storage.Get<DateTime>(storageKey);
-		    }
-		    catch (Exception)
-		    {
-		        startDate = settings.StartDate;
-		    }
+			try
+			{
+				startDate = _storage.Get<DateTime>(storageKey);
+			}
+			catch (Exception)
+			{
+				startDate = settings.StartDate;
+			}
 
-		    var result = new ReductionPeriod();
+			var result = new ReductionPeriod();
 
-		    result.From = new DateTime(startDate.Year, startDate.Month, startDate.Day, 0, 0, 0, 0);
-		    result.To = result.From.AddDays(1);
-		    result.IsCurrent = startDate.Date == DateTime.UtcNow.Date;
+			result.From = new DateTime(startDate.Year, startDate.Month, startDate.Day, 0, 0, 0, 0);
+			result.To = result.From.AddDays(1);
+			result.IsCurrent = startDate.Date == DateTime.UtcNow.Date;
 
 			return result;
 		}
 
-	    private string GetPositionStorageKey(ReductionSettings settings)
-	    {
-	        return "poistion_" + GetType().Name + "_" + settings.TimeInterval;
-	    }
-
-	    public void Stop()
+		private string GetPositionStorageKey(ReductionSettings settings)
 		{
-            if (_tokenSource == null) return;
+			return "poistion_" + GetType().Name + "_" + settings.TimeInterval;
+		}
 
-            Log.Info(string.Format("{0}: Stopping.", GetType().Name));
-            _tokenSource.Cancel();
+		public void Stop()
+		{
+			if (_tokenSource == null) return;
 
-            if (_processTask != null)
-            {
-                _processTask.Wait();
-            }
+			Log.Info(string.Format("{0}: Stopping.", GetType().Name));
+			_tokenSource.Cancel();
+
+			if (_processTask != null)
+			{
+				_processTask.Wait();
+			}
 		}
 
 		public bool Validate()
